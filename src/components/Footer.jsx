02f7d3e829cbb3fb89724083e6fbdabdf20af0b7
Brandon O'Connor <brandonleeoconnor10@gmail.com>
2025-06-02 16:17:43 +0200

    Footer component created

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer className='text-sm text-gray-light mt-8 pb-4'>
+        © 2025 Learner Lane. All right reserved.
+    </footer>
+  )
+}
+
+export default Footer
