b3883f2b6e1fa74378195c23fb43718eac022132
Brandon O'Connor <brandonleeoconnor10@gmail.com>
2025-06-02 16:17:04 +0200

    Title component styles updated

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -1,7 +1,7 @@
 
 const Title = () => {
   return (
-    <h1 className="text-5xl font-bold text-white font-orbitron">Learner Lane</h1>
+    <h1 className="text-5xl font-bold text-white font-orbitron text-center py-6">Learner Lane</h1>
   )
 }
 
