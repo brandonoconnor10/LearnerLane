853d34493fd9acddd46871e05928ba2bb167410a
Brandon O'Connor <brandonleeoconnor10@gmail.com>
2025-06-02 09:41:15 +0200

    Added more logging to understand why the API returns an empty array  at times
    Sometimes the request is successful( no errors) but the response is empty

--- src/data/fetchData.js
+++ src/data/fetchData.js
@@ -1,33 +1,46 @@
+import axios from 'axios'
+
 const API_KEY = import.meta.env.VITE_AIRTABLE_API_KEY;
 const BASE_ID = import.meta.env.VITE_AIRTABLE_BASE_ID;
 
-
-export const fetchData = async (filterField = '', filterValue = '',maxRecords = 100) => {
+export const fetchData = async (filterField1 = '', filterValue1 = '', filterField2 = '', filterValue2 = '', maxRecords = 100) => {
     
     try {
         if(!API_KEY || !BASE_ID){
             throw new Error('Airtable API key or Base ID is missing in .env')
         }
 
-        let url = `https://api.airtable.com/v0/${BASE_ID}/K53_Content?maxRecords=${maxRecords}`
+        let url = `https://api.airtable.com/v0/${BASE_ID}/K53_Content`;
+        let params = { maxRecords }
 
-        if(filterField && filterValue){
-            url += `&filterByFormula={${filterField}}='${filterValue}'`;
+        if(filterField1 && filterValue1){
+          let formula = `{${filterField1}}='${filterValue1}'`;
+          if(filterField2 && filterValue2){
+            formula = `AND({${filterField1}}='${filterValue1}', {${filterField2}}='${filterValue2}')`;
+          }
+          params.filterByFormula = formula;
+          console.log('Generated formula: ', formula)
         }
 
-        const response = await fetch(url, {
+        console.log('Fetching URL: ', url)
+        console.log('Fetching params: ', params)
+
+        const response = await axios(url, {
                 headers: {
                     Authorization: `Bearer ${API_KEY}`,
                     'Content-Type': 'application/json',
                 },
+                params,
             })
-        if(!response.ok) throw new Error(`HTTP error! status: ${response.status}`);
-        const data = await response.json();
-        return data.records || [];
+
+        console.log('Full Response: ', response.data)
+        console.log('Fetched Data: ', response.data.records)    
+        
+        return response.data.records || [];
         } 
     catch(error){
-        console.log('Error fetching data:', error)
+        console.log('Error fetching Airtable data:', error.message, error.response?.data)
         return []    
     }
 
-}
+};
