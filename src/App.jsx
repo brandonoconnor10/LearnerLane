eae6387e0ef6cf707bb96dedd9b7cd517e7aee01
Brandon O'Connor <brandonleeoconnor10@gmail.com>
2025-06-02 16:18:20 +0200

    Updated App component to have Router for Homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
-import React from 'react'
+import { BrowserRouter as Router } from 'react-router-dom'
+import HomePage from "./pages/HomePage"
 
 const App = () => {
   return (
-    <div>App</div>
+    <Router>
+      <div className='min-h-screen bg-navy-dark text-white'>
+        <HomePage/>
+      </div>
+    </Router>
+      
+  
   )
 }
 
