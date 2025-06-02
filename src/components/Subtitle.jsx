c674aa0ffbb4d43ef4a913e0912df96dac1c8d57
Brandon O'Connor <brandonleeoconnor10@gmail.com>
2025-06-02 15:30:41 +0200

    Created Subtitle Component for Home Page

---
+++ src/components/Subtitle.jsx
@@ -0,0 +1,9 @@
+
+const Subtitle = () => {
+  return (
+    <p className="text-xl text-gray-light font-inter mt-4">Welcome to Leaner Lane - your personal guide to acing the K53 Learner's Licence!
+    Start your journey with easy-to-follow sections covering Rules of The Road, Road Signs and Car Controls.</p>
+  )
+}
+
+export default Subtitle
