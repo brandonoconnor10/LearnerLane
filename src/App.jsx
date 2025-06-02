f64f39f87a8f385d3c0b73efa54b5892aa61afe6
Brandon O'Connor <brandonleeoconnor10@gmail.com>
2025-06-02 12:05:10 +0200

    Set up basic Routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 
 const App = () => {
   return (
-    <div>
-      <Home />
-    </div>
+    <Router>
+      <div>
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+        </Routes>
+      </div>
+    </Router>
+    
   );
 }
 
