c150dd8b5617b1a5d9e7235c894392e57ed7163c
Brandon O'Connor <brandonleeoconnor10@gmail.com>
2025-06-02 16:16:37 +0200

    Design HomePage with Title, Subtitle and RoadMap components

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,25 @@
+import Title from  '../components/Title'
+import Subtitle from '../components/Subtitle'
+import Roadmap from '../components/Roadmap'
+import Footer from '../components/Footer'
+
+const HomePage = () => {
+  return (
+    <div className='min-h-screen bg-navy-dark text-white flex flex-col items-center justify-start p-4'>
+      {/* Title and Subtitle at the top */}
+      <div className='w-full'>
+        <Title/>
+        <Subtitle/>
+      </div>
+
+      {/* Roadmap centered below */}
+      <div className='flex-1 flex items-center justify-center w-full'>
+        <Roadmap/>
+      </div>
+      {/* Footer */}
+      <Footer/>
+    </div>
+  )
+}
+
+export default HomePage
