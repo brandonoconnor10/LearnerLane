e535e96381e3d453b961995ba7281a3113b37fe4
Brandon O'Connor <brandonleeoconnor10@gmail.com>
2025-06-02 09:38:15 +0200

    Simplified teh App page to just return the Home component

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,11 @@
-import { useEffect } from "react"
-import { fetchData } from "./data/fetchData"
-import Home from './pages/Home'
-
+import Home from './pages/Home';
 
 const App = () => {
-
-  useEffect(() => {fetchData().then(data => console.log('App received: ',data))}, [])  
   return (
     <div>
       <Home />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
