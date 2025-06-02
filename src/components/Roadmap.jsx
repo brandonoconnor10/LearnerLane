aa29ed960e6a777030ff84793a462f8f3cd97835
Brandon O'Connor <brandonleeoconnor10@gmail.com>
2025-06-02 16:25:58 +0200

    Updated Roadmap Styles to have a singular vertical line running through instead of in between each section

--- src/components/Roadmap.jsx
+++ src/components/Roadmap.jsx
@@ -1,27 +1,25 @@
-
-
 const Roadmap = () => {
   const sections = [
     'Introduction',
     'Road Signs',
     'Rules of The Road',
     'Car Controls',
-  ];  
+  ];
 
   return (
-    <div className="flex flex-col items-center">
-        {sections.map((section, index)=>(
-            <div key={index} className="relative flex flex-col items-center">
-                {/* button */}
-                <button className="bg-gray-dark text-white font-rajdhani text-lg md:text-xl py-3 md:py-4 px-6 md:px-8 rounded-lg border-2 border-cyan hover:bg-cyan-light hover:border-cyan-light transition-colors duration-200 cursor-pointer">{section}</button>
-                {/* Connecting Line (except for the last line) */}
-                {index < sections.length - 1 && (
-                    <div className="w-1 h-6 md:h-8 bg-cyan my-2"></div>
-                )}
-            </div>
-        ))}
-    </div>
-  )
-}
+    <div className="relative flex flex-col items-center">
+      {/* Vertical Line running through the center */}
+      <div className="absolute top-0 left-1/2 transform -translate-x-1/2 h-full w-1 bg-cyan z-0" />
 
-export default Roadmap
+      {sections.map((section, index) => (
+        <div key={index} className="relative z-10 flex flex-col items-center my-6">
+          <button className="bg-gray-dark text-white font-rajdhani text-lg md:text-xl py-3 md:py-4 px-6 md:px-8 rounded-lg border-2 border-cyan hover:bg-cyan-light hover:border-cyan-light transition-colors duration-200 cursor-pointer">
+            {section}
+          </button>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Roadmap;
