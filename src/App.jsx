e30da327726065e35522ef729f26e4933d8aa088
Brandon O'Connor <brandonleeoconnor10@gmail.com>
2025-06-03 11:14:23 +0200

    Updated Imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
-import { BrowserRouter as Router } from 'react-router-dom'
-import HomePage from "./pages/HomePage"
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import HomePage from './pages/HomePage';
 
 const App = () => {
   return (
     <Router>
-      <div className='min-h-screen bg-navy-dark text-white'>
-        <HomePage/>
+      <div className="min-h-screen bg-navy-dark text-white">
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+        </Routes>
       </div>
     </Router>
-      
-  
-  )
+  );
 }
 
-export default App
+export default App;
