6e4a1d26cd56ba1937be3c572399c245fbf1dba6
Brandon O'Connor <brandonleeoconnor10@gmail.com>
2025-06-05 20:32:38 +0200

    Added a Route to the Introduction Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
+import SectionPage from './pages/SectionPage';
+import IntroductionPage from './pages/IntroductionPage';
 
 const App = () => {
   return (
@@ -7,6 +9,8 @@ const App = () => {
       <div className="min-h-screen bg-navy-dark text-white">
         <Routes>
           <Route path="/" element={<HomePage />} />
+          <Route path="/introduction" element={<IntroductionPage />} />
+          <Route path="/:section" element={<SectionPage />} />
         </Routes>
       </div>
     </Router>
