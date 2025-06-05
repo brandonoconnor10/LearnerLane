1c10063abc8c23420633a8da8b675269a57ddcf7
Brandon O'Connor <brandonleeoconnor10@gmail.com>
2025-06-05 20:49:11 +0200

    Removed SectionPage for now

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
-import SectionPage from './pages/SectionPage';
 import IntroductionPage from './pages/IntroductionPage';
 
 const App = () => {
@@ -10,7 +9,6 @@ const App = () => {
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/introduction" element={<IntroductionPage />} />
-          <Route path="/:section" element={<SectionPage />} />
         </Routes>
       </div>
     </Router>
