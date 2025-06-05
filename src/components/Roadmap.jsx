34fd862ae837be169fd8ff84e64f4cc1115d0f1a
Brandon O'Connor <brandonleeoconnor10@gmail.com>
2025-06-05 20:37:54 +0200

    Changed the Line design to only have singular vertical line

--- src/components/Roadmap.jsx
+++ src/components/Roadmap.jsx
@@ -9,15 +9,15 @@ function RoadMap() {
   ];
 
   return (
-    <div className="relative flex flex-col items-center py-20 min-h-[80vh]">
-      {/* Vertical line running through the center */}
-      <div className="absolute top-0 left-1/2 transform -translate-x-1/2 h-full w-1.5 bg-cyan z-0" />
+    <div className="flex flex-col items-center relative">
+      {/* Continuous Vertical Line */}
+      <div className="absolute w-1 bg-cyan top-[-10px] bottom-[-10px] left-1/2 transform -translate-x-1/2 z-0"></div>
 
       {sections.map((section, index) => (
-        <div key={index} className="relative z-10 flex flex-col items-center my-10">
+        <div key={index} className="relative flex flex-col items-center z-10">
           <Link
-            to={`/${section.toLowerCase().replace(/\s+/g, '-')}`}
-            className="bg-gray-dark text-white font-rajdhani text-lg md:text-xl py-3 md:py-4 px-6 md:px-8 rounded-lg border-2 border-cyan hover:bg-cyan-light hover:border-cyan-light transition-colors duration-200 cursor-pointer text-center"
+            to={`/${section.toLowerCase().replace(/\s+/g, '-')}`} 
+            className="bg-gray-dark text-white font-rajdhani text-lg md:text-xl py-3 md:py-4 px-6 md:px-8 rounded-lg border-2 border-cyan hover:bg-cyan-light hover:border-cyan-light transition-colors duration-200 cursor-pointer text-center my-6"
           >
             {section}
           </Link>
