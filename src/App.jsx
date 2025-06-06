73c9dc5311d8bb4c448179e18976b283fcfde4ae
Brandon O'Connor <brandonleeoconnor10@gmail.com>
2025-06-06 18:08:24 +0200

    Added Route to K53 Explained Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
+
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import IntroductionPage from './pages/IntroductionPage';
+import K53ExplainedPage from './pages/K53ExplainedPage';
 
 const App = () => {
   return (
@@ -9,10 +11,11 @@ const App = () => {
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/introduction" element={<IntroductionPage />} />
+          <Route path="/k53-explained" element={<K53ExplainedPage />} />
         </Routes>
       </div>
     </Router>
   );
-}
+};
 
 export default App;
