eb0ac84bd6f6d9de0d5ed6190e33847f910a25d1
Brandon O'Connor <brandonleeoconnor10@gmail.com>
2025-06-06 20:42:53 +0200

    Added Route to k53 Explained Quiz Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
-
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import IntroductionPage from './pages/IntroductionPage';
 import K53ExplainedPage from './pages/K53ExplainedPage';
+import K53ExplainedQuizPage from './pages/K53ExplainedQuizPage';
 
 const App = () => {
   return (
@@ -12,6 +12,7 @@ const App = () => {
           <Route path="/" element={<HomePage />} />
           <Route path="/introduction" element={<IntroductionPage />} />
           <Route path="/k53-explained" element={<K53ExplainedPage />} />
+          <Route path="/k53-explained/quiz" element={<K53ExplainedQuizPage />} />
         </Routes>
       </div>
     </Router>
