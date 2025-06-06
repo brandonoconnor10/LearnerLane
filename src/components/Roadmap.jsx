fced23b8edcec658940c43cdf91be8edba00bf39
Brandon O'Connor <brandonleeoconnor10@gmail.com>
2025-06-06 15:47:46 +0200

    Update RoadMap Component

--- src/components/Roadmap.jsx
+++ src/components/Roadmap.jsx
@@ -1,4 +1,5 @@
-import { Link } from 'react-router-dom';
+import VerticalLineContainer from './VerticalLineContainer';
+import StyledButton from './StyledButton';
 
 function RoadMap() {
   const sections = [
@@ -9,21 +10,20 @@ function RoadMap() {
   ];
 
   return (
-    <div className="flex flex-col items-center relative">
-      {/* Continuous Vertical Line */}
-      <div className="absolute w-1 bg-cyan top-[-10px] bottom-[-10px] left-1/2 transform -translate-x-1/2 z-0"></div>
-
+    <VerticalLineContainer>
       {sections.map((section, index) => (
         <div key={index} className="relative flex flex-col items-center z-10">
-          <Link
-            to={`/${section.toLowerCase().replace(/\s+/g, '-')}`} 
-            className="bg-gray-dark text-white font-rajdhani text-lg md:text-xl py-3 md:py-4 px-6 md:px-8 rounded-lg border-2 border-cyan hover:bg-cyan-light hover:border-cyan-light transition-colors duration-200 cursor-pointer text-center my-6"
+          <StyledButton
+            to={`/${section.toLowerCase().replace(/\s+/g, '-')}`}
+            asLink
+            variant="small"
+            className="my-6"
           >
             {section}
-          </Link>
+          </StyledButton>
         </div>
       ))}
-    </div>
+    </VerticalLineContainer>
   );
 }
 
