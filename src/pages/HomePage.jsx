982de17869f6b43118fc9c22c543c8ee8f7fbfd1
Brandon O'Connor <brandonleeoconnor10@gmail.com>
2025-06-06 12:54:28 +0200

    Updated HomePage to use the other components to lessen the code in it

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,24 +1,13 @@
-import Title from  '../components/Title'
+import PageLayout from '../components/PageLayout'
 import Subtitle from '../components/Subtitle'
-import Roadmap from '../components/RoadMap'
-import Footer from '../components/Footer'
+import RoadMap from '../components/RoadMap'
+
 
 const HomePage = () => {
   return (
-    <div className='min-h-screen bg-navy-dark text-white flex flex-col items-center justify-start p-4'>
-      {/* Title and Subtitle at the top */}
-      <div className='w-full'>
-        <Title/>
-        <Subtitle/>
-      </div>
-
-      {/* Roadmap centered below */}
-      <div className='flex-1 flex items-center justify-center w-full'>
-        <Roadmap/>
-      </div>
-      {/* Footer */}
-      <Footer/>
-    </div>
+    <PageLayout subtitle={<Subtitle />}>
+      <RoadMap />
+    </PageLayout>
   )
 }
 
