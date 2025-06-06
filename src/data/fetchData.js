3efe637dacd3025752b6fc13c48117a53cd38f16
Brandon O'Connor <brandonleeoconnor10@gmail.com>
2025-06-06 20:43:34 +0200

    Updated fetchData to have more fields to fetch , in this case the option and answer fields need to be returned

--- src/data/fetchData.js
+++ src/data/fetchData.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const API_KEY = import.meta.env.VITE_AIRTABLE_API_KEY;
 const BASE_ID = import.meta.env.VITE_AIRTABLE_BASE_ID;
 
-export const fetchData = async (filterField1 = '', filterValue1 = '', filterField2 = '', filterValue2 = '', maxRecords = 100) => {
+export const fetchData = async (filterField1 = '', filterValue1 = '', filterField2 = '', filterValue2 = '', maxRecords = 100, fields = []) => {
   try {
     if (!API_KEY || !BASE_ID) {
       throw new Error('Airtable API key or Base ID is missing in .env');
@@ -13,17 +13,19 @@ export const fetchData = async (filterField1 = '', filterValue1 = '', filterFiel
     let params = { maxRecords };
 
     if (filterField1 && filterValue1) {
-      let formula = `{${filterField1}}=${filterValue1}`; 
+      let formula = `{${filterField1}}=${filterValue1}`;
       if (filterField2 && filterValue2) {
         formula = `AND({${filterField1}}=${filterValue1}, {${filterField2}}=${filterValue2})`;
       }
       params.filterByFormula = formula;
-      params.fields = ['Section', 'Subsection', 'Content', 'Type']; 
-      console.log('Generated formula:', formula);
-      console.log('Final params:', params); 
+    }
+
+    if (fields.length > 0) {
+      params.fields = fields; 
     }
 
     console.log('Fetching URL:', url);
+    console.log('Final params:', params);
 
     const response = await axios.get(url, {
       headers: {
