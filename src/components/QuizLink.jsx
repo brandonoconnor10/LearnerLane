124fd0aeb4ec11fabf607bbd33b856caf273043c
Brandon O'Connor <brandonleeoconnor10@gmail.com>
2025-06-06 15:47:31 +0200

    Update Quiz Link Component

--- src/components/QuizLink.jsx
+++ src/components/QuizLink.jsx
@@ -1,26 +1,28 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 const QuizLink = ({ section, index }) => {
-    
-    const isEven = index % 2 === 0;
-    const linkPath = `/${section.toLowerCase().replace(/\s+/g, '-').replace(/'/g, '')}/quiz`;
-    
-  return (
-   <div className='relative flex items-center w-full justify-center mb-6 mt-2'>
-    <div className={`absolute h-0.5 bg-cyan z-0 ${
-        isEven ? 'left-{calc(50%) right-0' : 'left-0 right-[calc(50%)]'}
-    }`}></div>
-    <Link to={linkPath} className={`absolute text-cyan text-sm md:text-base font-inter ${
-        isEven ? 'right-0 text-right' : 'left-0'
-    }`}
-    style={{ transform: isEven ? 'translateX(120%)' : 'translateX(-120%)'}}
-    >
-    Quiz
-    </Link>
-    
-    </div>
-  )
-}
+  const isEven = index % 2 === 0;
+  const linkPath = `/${section.toLowerCase().replace(/\s+/g, '-').replace(/'/g, '')}/quiz`;
 
-export default QuizLink
+  return (
+    <div className="relative flex items-center w-full justify-center mb-6 mt-2 z-10">
+      <div
+        className={`absolute h-0.5 bg-cyan z-0 ${
+          isEven ? 'left-[calc(50%)] right-0' : 'left-0 right-[calc(50%)]'
+        }`}
+      ></div>
+      <Link
+        to={linkPath}
+        className={`absolute text-sm md:text-base font-inter z-20 ${
+          isEven ? 'right-0 text-right' : 'left-0'
+        }`}
+        style={{ transform: isEven ? 'translateX(150%)' : 'translateX(-150%)', color: '#b0b8c1' }}
+      >
+        Quiz
+      </Link>
+    </div>
+  );
+};
+
+export default QuizLink;
