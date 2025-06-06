ed1362f1c6187be80009e34b2ae6f750fef744c4
Brandon O'Connor <brandonleeoconnor10@gmail.com>
2025-06-06 15:48:41 +0200

    Fixed Vertical Line Container Component

--- src/components/VerticalLineContainer.jsx
+++ src/components/VerticalLineContainer.jsx
@@ -1,12 +1,12 @@
-import React from 'react'
+import React from 'react';
 
-const VerticalLineContainer = ({ children }) => {
+const VerticalLineContainer = ({ children, className = '' }) => {
   return (
-    <div className='flex flex-col items-center relative w-full max-w-md'>
-        <div className="absolute w-1 bg-cyan top-[-10px] left-1/2 transform -translate-x-1/2 z-0"></div>
-        {children}
+    <div className={`flex flex-col items-center relative w-full ${className}`}>
+      <div className="absolute w-1 bg-cyan top-[-10px] bottom-[-10px] left-1/2 transform -translate-x-1/2 z-0"></div>
+      {children}
     </div>
-  )
-}
+  );
+};
 
-export default VerticalLineContainer
+export default VerticalLineContainer;
