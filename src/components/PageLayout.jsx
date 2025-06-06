30104d710a105b3cd5694ee91fd81a96051fab25
Brandon O'Connor <brandonleeoconnor10@gmail.com>
2025-06-06 15:47:14 +0200

    Updated Page Layout Component

--- src/components/PageLayout.jsx
+++ src/components/PageLayout.jsx
@@ -1,20 +1,20 @@
-import React from 'react'
-import Title from '../components/Title'
-import Footer from '../components/Footer'
+import React from 'react';
+import Title from './Title';
+import Footer from './Footer';
 
-const PageLayout = ({ subtitle, children }) => {
+const PageLayout = ({ subtitle, children, contentClassName = 'items-center justify-center' }) => {
   return (
-    <div className='min-h-screen bg-navy-dark text-white flex flex-col items-center justify-start p-4'>
-        <div className="w-full text center">
-            <Title/>
-            {subtitle && subtitle}
-        </div>
-        <div className="flex-1 flex items-center justify-center w-full">
-            {children}
-        </div>
-        <Footer/>
+    <div className="min-h-screen bg-navy-dark text-white flex flex-col items-center justify-start p-4">
+      <div className="w-full text-center">
+        <Title />
+        {subtitle && subtitle}
+      </div>
+      <div className={`flex-1 flex flex-col w-full ${contentClassName}`}>
+        {children}
+      </div>
+      <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default PageLayout
+export default PageLayout;
