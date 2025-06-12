6c511487a0b74d0f75aadf54d43843ed81fd6dbd
Brandon O'Connor <brandonleeoconnor10@gmail.com>
2025-06-12 21:24:23 +0200

    Update SelectiveRestrictionSignsPage.jsx

--- src/pages/SelectiveRestrictionSignsPage.jsx
+++ src/pages/SelectiveRestrictionSignsPage.jsx
@@ -89,6 +89,8 @@ export default function SelectiveRestrictionSignsPage() {
         ))}
         <div className="relative flex flex-col items-center z-10">
           <StyledButton
+            to="/selective-restriction-signs/quiz"
+            asLink
             variant="large"
             className="my-6"
           >
