8ff1c14e49d0180781c0a345e66965b2bc1ca47d
Brandon O'Connor <brandonleeoconnor10@gmail.com>
2025-06-12 20:44:10 +0200

    Link to quiz page

--- src/pages/ProhibitionSignsPage.jsx
+++ src/pages/ProhibitionSignsPage.jsx
@@ -89,6 +89,8 @@ export default function ProhibitionSignsPage() {
         ))}
         <div className="relative flex flex-col items-center z-10">
           <StyledButton
+            to="/prohibition-signs/quiz"
+            asLink
             variant="large"
             className="my-6"
           >
