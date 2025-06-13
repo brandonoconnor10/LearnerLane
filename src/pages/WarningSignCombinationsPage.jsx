d04979b387f4ac3c05aa5b739af0e04a7c479d2b
Brandon O'Connor <brandonleeoconnor10@gmail.com>
2025-06-13 12:00:14 +0200

    Link to quiz page

--- src/pages/WarningSignCombinationsPage.jsx
+++ src/pages/WarningSignCombinationsPage.jsx
@@ -89,6 +89,8 @@ export default function WarningSignCombinationsPage() {
         ))}
         <div className="relative flex flex-col items-center z-10">
           <StyledButton
+            to="/warning-sign-combinations-signs/quiz"
+            asLink
             variant="large"
             className="my-6"
           >
