42b2ecc1d20dec35efb3a778567bc0bd11aecd65
Brandon O'Connor <brandonleeoconnor10@gmail.com>
2025-06-13 13:34:32 +0200

    Link to Quiz Page

--- src/pages/InformationSignsPage.jsx
+++ src/pages/InformationSignsPage.jsx
@@ -89,6 +89,8 @@ export default function InformationSignsPage() {
         ))}
         <div className="relative flex flex-col items-center z-10">
           <StyledButton
+            to="/information-signs/quiz"
+            asLink
             variant="large"
             className="my-6"
           >
