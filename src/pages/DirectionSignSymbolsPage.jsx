f12270c6548207399c3cd7647418bea624be2314
Brandon O'Connor <brandonleeoconnor10@gmail.com>
2025-06-13 13:34:09 +0200

    Link to Quiz Page

--- src/pages/DirectionSignSymbolsPage.jsx
+++ src/pages/DirectionSignSymbolsPage.jsx
@@ -89,6 +89,8 @@ export default function DirectionSignSymbolsPage() {
         ))}
         <div className="relative flex flex-col items-center z-10">
           <StyledButton
+            to="/direction-sign-symbols/quiz"
+            asLink
             variant="large"
             className="my-6"
           >
