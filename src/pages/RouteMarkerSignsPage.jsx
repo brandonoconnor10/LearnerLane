00beb972d3c0565dc43e21abdfa74a9ba98fc34a
Brandon O'Connor <brandonleeoconnor10@gmail.com>
2025-06-13 13:35:11 +0200

    Link to Quiz Page

--- src/pages/RouteMarkerSignsPage.jsx
+++ src/pages/RouteMarkerSignsPage.jsx
@@ -89,6 +89,8 @@ export default function RouteMarkerSignsPage() {
         ))}
         <div className="relative flex flex-col items-center z-10">
           <StyledButton
+            to="/route-marker-signs/quiz"
+            asLink
             variant="large"
             className="my-6"
           >
