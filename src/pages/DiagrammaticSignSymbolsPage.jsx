1b1083ba6a40cf1630b147377289debe1cb6d386
Brandon O'Connor <brandonleeoconnor10@gmail.com>
2025-06-13 13:33:42 +0200

    Link to Quiz Page

--- src/pages/DiagrammaticSignSymbolsPage.jsx
+++ src/pages/DiagrammaticSignSymbolsPage.jsx
@@ -89,6 +89,8 @@ export default function DiagrammaticSignSymbolsPage() {
         ))}
         <div className="relative flex flex-col items-center z-10">
           <StyledButton
+            to="/diagrammatic-sign-symbols/quiz"
+            asLink
             variant="large"
             className="my-6"
           >
