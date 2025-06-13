8c507beb89a1b9a46dac5409f1f4c9802618c4fe
Brandon O'Connor <brandonleeoconnor10@gmail.com>
2025-06-13 11:59:34 +0200

    Link to quiz page

--- src/pages/DirectionOfMovementSignsPage.jsx
+++ src/pages/DirectionOfMovementSignsPage.jsx
@@ -89,6 +89,8 @@ export default function DirectionOfMovementSignsPage() {
         ))}
         <div className="relative flex flex-col items-center z-10">
           <StyledButton
+            to="/direction-of-movement-signs/quiz"
+            asLink
             variant="large"
             className="my-6"
           >
