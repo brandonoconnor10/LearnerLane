f75f7552a01133619f95c9608d5a8dbc95e03cd9
Brandon O'Connor <brandonleeoconnor10@gmail.com>
2025-06-14 09:20:24 +0200

    Update TemporaryHazardMarkerSignsPage.jsx

--- src/pages/TemporaryHazardMarkerSignsPage.jsx
+++ src/pages/TemporaryHazardMarkerSignsPage.jsx
@@ -89,6 +89,8 @@ export default function TemporaryHazardMarkerSignsPage() {
         ))}
         <div className="relative flex flex-col items-center z-10">
           <StyledButton
+            to='/temporary-hazard-marker-signs/quiz'
+            asLink
             variant="large"
             className="my-6"
           >
