2dfbe4f4bfe2d6ed3ed916e726e930665cf59ce9
Brandon O'Connor <brandonleeoconnor10@gmail.com>
2025-06-14 09:20:45 +0200

    Update TemporaryReservationSignsPage.jsx

--- src/pages/TemporaryReservationSignsPage.jsx
+++ src/pages/TemporaryReservationSignsPage.jsx
@@ -89,6 +89,8 @@ export default function TemporaryReservationSignsPage() {
         ))}
         <div className="relative flex flex-col items-center z-10">
           <StyledButton
+            to="/temporary-reservation-signs/quiz"
+            asLink
             variant="large"
             className="my-6"
           >
