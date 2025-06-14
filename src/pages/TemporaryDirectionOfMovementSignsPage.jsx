ff2ac311d1a7ef960b0fdbb4efc1ffeccf127ade
Brandon O'Connor <brandonleeoconnor10@gmail.com>
2025-06-14 09:20:07 +0200

    Update TemporaryDirectionOfMovementSignsPage.jsx

--- src/pages/TemporaryDirectionOfMovementSignsPage.jsx
+++ src/pages/TemporaryDirectionOfMovementSignsPage.jsx
@@ -89,6 +89,8 @@ export default function TemporaryDirectionOfMovementSignsPage() {
         ))}
         <div className="relative flex flex-col items-center z-10">
           <StyledButton
+            to="/temporary-direction-of-movement-signs/quiz"
+            asLink
             variant="large"
             className="my-6"
           >
