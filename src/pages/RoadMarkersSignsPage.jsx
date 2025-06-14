976e986dc76521461fd8c3a0c20f85341456435f
Brandon O'Connor <brandonleeoconnor10@gmail.com>
2025-06-14 09:19:44 +0200

    Update RoadMarkersSignsPage.jsx

--- src/pages/RoadMarkersSignsPage.jsx
+++ src/pages/RoadMarkersSignsPage.jsx
@@ -89,6 +89,8 @@ export default function RoadMarkersSignPage() {
         ))}
         <div className="relative flex flex-col items-center z-10">
           <StyledButton
+            to="/road-markers/quiz"
+            asLink
             variant="large"
             className="my-6"
           >
