9c0d6915549d295e42327eef553c2dbb4742799f
Brandon O'Connor <brandonleeoconnor10@gmail.com>
2025-06-15 09:57:05 +0200

    Update OvertakingPage.jsx

--- src/pages/OvertakingPage.jsx
+++ src/pages/OvertakingPage.jsx
@@ -43,7 +43,7 @@ const OvertakingPage = () => {
         </div>
 
         <StyledButton
-          to="/k53-explained/quiz"
+          to="/overtaking-or-passing-another-vehicle/quiz"
           asLink
           variant="large"
           className="mt-32 mb-6"
