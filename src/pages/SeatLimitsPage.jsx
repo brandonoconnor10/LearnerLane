3f96abb9018f1a5bc9f78f993c83154bf4dda719
Brandon O'Connor <brandonleeoconnor10@gmail.com>
2025-06-15 09:59:12 +0200

    Update SeatLimitsPage.jsx

--- src/pages/SeatLimitsPage.jsx
+++ src/pages/SeatLimitsPage.jsx
@@ -43,7 +43,7 @@ const SeatLimitsPage = () => {
         </div>
 
         <StyledButton
-          to="/k53-explained/quiz"
+          to="/seat-limits/quiz"
           asLink
           variant="large"
           className="mt-32 mb-6"
