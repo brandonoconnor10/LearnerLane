79c8f0795dd33078879127ef65bc5767018d58ae
Brandon O'Connor <brandonleeoconnor10@gmail.com>
2025-06-15 09:52:55 +0200

    Update AlcoholAndNarcoticDrugsPage.jsx

--- src/pages/AlcoholAndNarcoticDrugsPage.jsx
+++ src/pages/AlcoholAndNarcoticDrugsPage.jsx
@@ -43,7 +43,7 @@ const AlcoholAndNarcoticDrugsPage = () => {
         </div>
 
         <StyledButton
-          to="/k53-explained/quiz"
+          to="/alcohol-and-narcotic-drugs/quiz"
           asLink
           variant="large"
           className="mt-32 mb-6"
