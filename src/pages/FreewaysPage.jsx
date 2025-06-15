67cbc6673799c94ff6ab0d91f08009c82db85934
Brandon O'Connor <brandonleeoconnor10@gmail.com>
2025-06-15 09:55:23 +0200

    Update FreewaysPage.jsx

--- src/pages/FreewaysPage.jsx
+++ src/pages/FreewaysPage.jsx
@@ -43,7 +43,7 @@ const FreewaysPage = () => {
         </div>
 
         <StyledButton
-          to="/k53-explained/quiz"
+          to="/freeways/quiz"
           asLink
           variant="large"
           className="mt-32 mb-6"
