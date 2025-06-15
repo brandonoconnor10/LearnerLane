9efd0603103b6208d25e965a45a648ba1425e928
Brandon O'Connor <brandonleeoconnor10@gmail.com>
2025-06-15 10:00:03 +0200

    Update TrianglesPage.jsx

--- src/pages/TrianglesPage.jsx
+++ src/pages/TrianglesPage.jsx
@@ -43,7 +43,7 @@ const TrianglesPage = () => {
         </div>
 
         <StyledButton
-          to="/k53-explained/quiz"
+          to="/triangles/quiz"
           asLink
           variant="large"
           className="mt-32 mb-6"
