cbcc723cd4b483119c9a03161ee97240b1c2b9a6
Brandon O'Connor <brandonleeoconnor10@gmail.com>
2025-06-15 09:55:57 +0200

    Update ImportantRulesPage.jsx

--- src/pages/ImportantRulesPage.jsx
+++ src/pages/ImportantRulesPage.jsx
@@ -43,7 +43,7 @@ const ImportantRulesPage = () => {
         </div>
 
         <StyledButton
-          to="/k53-explained/quiz"
+          to="/important-rules/quiz"
           asLink
           variant="large"
           className="mt-32 mb-6"
