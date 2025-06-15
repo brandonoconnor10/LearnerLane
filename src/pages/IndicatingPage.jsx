3b39913073632fba167aaa158027956518076217
Brandon O'Connor <brandonleeoconnor10@gmail.com>
2025-06-15 09:56:03 +0200

    Update IndicatingPage.jsx

--- src/pages/IndicatingPage.jsx
+++ src/pages/IndicatingPage.jsx
@@ -43,7 +43,7 @@ const IndicatingPage = () => {
         </div>
 
         <StyledButton
-          to="/k53-explained/quiz"
+          to="/indicating/quiz"
           asLink
           variant="large"
           className="mt-32 mb-6"
