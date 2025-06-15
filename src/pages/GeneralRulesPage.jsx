12706ccbac253ae4b607a37095b0687ed1617b83
Brandon O'Connor <brandonleeoconnor10@gmail.com>
2025-06-15 09:55:41 +0200

    Update GeneralRulesPage.jsx

--- src/pages/GeneralRulesPage.jsx
+++ src/pages/GeneralRulesPage.jsx
@@ -43,7 +43,7 @@ const GeneralRulesPage = () => {
         </div>
 
         <StyledButton
-          to="/k53-explained/quiz"
+          to="/general-rules/quiz"
           asLink
           variant="large"
           className="mt-32 mb-6"
