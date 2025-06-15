18964ee67ff4bbb37ffb2b9c1a9fdbd1ce4a5604
Brandon O'Connor <brandonleeoconnor10@gmail.com>
2025-06-15 09:53:21 +0200

    Update BeingOvertakenPage.jsx

--- src/pages/BeingOvertakenPage.jsx
+++ src/pages/BeingOvertakenPage.jsx
@@ -43,7 +43,7 @@ const BeingOvertakenPage = () => {
         </div>
 
         <StyledButton
-          to="/k53-explained/quiz"
+          to="/being-overtaken/quiz"
           asLink
           variant="large"
           className="mt-32 mb-6"
