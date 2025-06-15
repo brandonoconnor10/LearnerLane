8e1d4d004c32615a7a8915ca9ebc50bb88c8943e
Brandon O'Connor <brandonleeoconnor10@gmail.com>
2025-06-15 09:58:29 +0200

    Update RacingOnPublicRoadsPage.jsx

--- src/pages/RacingOnPublicRoadsPage.jsx
+++ src/pages/RacingOnPublicRoadsPage.jsx
@@ -43,7 +43,7 @@ const RacingOnPublicRoadsPage = () => {
         </div>
 
         <StyledButton
-          to="/k53-explained/quiz"
+          to="/racing-and-sport-on-public-roads/quiz"
           asLink
           variant="large"
           className="mt-32 mb-6"
