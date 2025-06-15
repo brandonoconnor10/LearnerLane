1325507e57af55310deeb58801ed33be5513dc0f
Brandon O'Connor <brandonleeoconnor10@gmail.com>
2025-06-15 09:59:57 +0200

    Update TowingPage.jsx

--- src/pages/TowingPage.jsx
+++ src/pages/TowingPage.jsx
@@ -43,7 +43,7 @@ const TowingPage = () => {
         </div>
 
         <StyledButton
-          to="/k53-explained/quiz"
+          to="/towing/quiz"
           asLink
           variant="large"
           className="mt-32 mb-6"
