4d064110cef032016c50ba8a62a7ed3399708a9f
Brandon O'Connor <brandonleeoconnor10@gmail.com>
2025-06-15 10:00:44 +0200

    Update VehiclesCausingExcessiveNoisePage.jsx

--- src/pages/VehiclesCausingExcessiveNoisePage.jsx
+++ src/pages/VehiclesCausingExcessiveNoisePage.jsx
@@ -43,7 +43,7 @@ const VehiclesCausingExcessiveNoisePage = () => {
         </div>
 
         <StyledButton
-          to="/k53-explained/quiz"
+          to="/vehicles-causing-excessive-noise/quiz"
           asLink
           variant="large"
           className="mt-32 mb-6"
