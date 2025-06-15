ddf359005c68ef5ec68c5c3c460e2ce8ccc97875
Brandon O'Connor <brandonleeoconnor10@gmail.com>
2025-06-15 09:58:18 +0200

    Update ProcessOfDefensiveDrivingPage.jsx

--- src/pages/ProcessOfDefensiveDrivingPage.jsx
+++ src/pages/ProcessOfDefensiveDrivingPage.jsx
@@ -43,7 +43,7 @@ const ProcessOfDefensiveDrivingPage = () => {
         </div>
 
         <StyledButton
-          to="/k53-explained/quiz"
+          to="/the-process-of-defensive-driving/quiz"
           asLink
           variant="large"
           className="mt-32 mb-6"
