3e2dde1cdc12cac1c32fe669db2eee5d71222579
Brandon O'Connor <brandonleeoconnor10@gmail.com>
2025-06-15 09:57:15 +0200

    Update ParkingPage.jsx

--- src/pages/ParkingPage.jsx
+++ src/pages/ParkingPage.jsx
@@ -43,7 +43,7 @@ const ParkingPage = () => {
         </div>
 
         <StyledButton
-          to="/k53-explained/quiz"
+          to="/parking/quiz"
           asLink
           variant="large"
           className="mt-32 mb-6"
