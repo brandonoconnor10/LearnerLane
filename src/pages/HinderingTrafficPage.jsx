ee49715a9049ddc13b26cc40be18e22e7b1dc587
Brandon O'Connor <brandonleeoconnor10@gmail.com>
2025-06-15 09:55:50 +0200

    Update HinderingTrafficPage.jsx

--- src/pages/HinderingTrafficPage.jsx
+++ src/pages/HinderingTrafficPage.jsx
@@ -43,7 +43,7 @@ const HinderingTrafficPage = () => {
         </div>
 
         <StyledButton
-          to="/k53-explained/quiz"
+          to="/hindering-or-obstructing-traffic-on-a-public-road/quiz"
           asLink
           variant="large"
           className="mt-32 mb-6"
