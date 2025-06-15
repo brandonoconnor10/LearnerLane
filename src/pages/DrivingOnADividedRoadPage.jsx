dccf153259b44eeaead65701bf7a0029e4aede69
Brandon O'Connor <brandonleeoconnor10@gmail.com>
2025-06-15 09:54:31 +0200

    Update DrivingOnADividedRoadPage.jsx

--- src/pages/DrivingOnADividedRoadPage.jsx
+++ src/pages/DrivingOnADividedRoadPage.jsx
@@ -43,7 +43,7 @@ const DrivingOnADividedRoadPage = () => {
         </div>
 
         <StyledButton
-          to="/k53-explained/quiz"
+          to="/driving-on-a-divided-road/quiz"
           asLink
           variant="large"
           className="mt-32 mb-6"
