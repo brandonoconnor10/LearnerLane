2c3fc30f61788e29cccb417167ec8a797f6a3824
Brandon O'Connor <brandonleeoconnor10@gmail.com>
2025-06-15 09:58:02 +0200

    Update PedestrianCrossingPage.jsx

--- src/pages/PedestrianCrossingPage.jsx
+++ src/pages/PedestrianCrossingPage.jsx
@@ -43,7 +43,7 @@ const PedestrianCrossingPage = () => {
         </div>
 
         <StyledButton
-          to="/k53-explained/quiz"
+          to="/pedestrian-crossing/quiz"
           asLink
           variant="large"
           className="mt-32 mb-6"
