9826e5c7c4390ffdeae2cd81f2d9f6c1b8668f07
Brandon O'Connor <brandonleeoconnor10@gmail.com>
2025-06-15 09:53:35 +0200

    Update CarryingALoadPage.jsx

--- src/pages/CarryingALoadPage.jsx
+++ src/pages/CarryingALoadPage.jsx
@@ -43,7 +43,7 @@ const CarryingALoadPage = () => {
         </div>
 
         <StyledButton
-          to="/k53-explained/quiz"
+          to="/carrying-a-load/quiz"
           asLink
           variant="large"
           className="mt-32 mb-6"
