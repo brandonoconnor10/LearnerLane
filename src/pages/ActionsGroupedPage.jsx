f2b5ae09d989dc7a929d2b7aa92179ac3e2135eb
Brandon O'Connor <brandonleeoconnor10@gmail.com>
2025-06-15 09:52:45 +0200

    Update ActionsGroupedPage.jsx

--- src/pages/ActionsGroupedPage.jsx
+++ src/pages/ActionsGroupedPage.jsx
@@ -43,7 +43,7 @@ const ActionsGroupedPage = () => {
         </div>
 
         <StyledButton
-          to="/k53-explained/quiz"
+          to="/actions-grouped/quiz"
           asLink
           variant="large"
           className="mt-32 mb-6"
