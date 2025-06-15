b01f272e5aa377f5b128262d22e6a7cb0e4ab41e
Brandon O'Connor <brandonleeoconnor10@gmail.com>
2025-06-15 09:59:20 +0200

    Update SpeedLimitsPage.jsx

--- src/pages/SpeedLimitsPage.jsx
+++ src/pages/SpeedLimitsPage.jsx
@@ -43,7 +43,7 @@ const SpeedLimitsPage = () => {
         </div>
 
         <StyledButton
-          to="/k53-explained/quiz"
+          to="/speed-limits/quiz"
           asLink
           variant="large"
           className="mt-32 mb-6"
