0c67a0b96e5359f801ad47bd83d8234f7e7cda56
Brandon O'Connor <brandonleeoconnor10@gmail.com>
2025-06-15 10:00:39 +0200

    Update VehicleAccidentsPage.jsx

--- src/pages/VehicleAccidentsPage.jsx
+++ src/pages/VehicleAccidentsPage.jsx
@@ -43,7 +43,7 @@ const VehicleAccidentsPage = () => {
         </div>
 
         <StyledButton
-          to="/k53-explained/quiz"
+          to="/vehicle-accidents/quiz"
           asLink
           variant="large"
           className="mt-32 mb-6"
