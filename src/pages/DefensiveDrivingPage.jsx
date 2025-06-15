1f04be8299f4c8dac9111196140b549c2d570bd9
Brandon O'Connor <brandonleeoconnor10@gmail.com>
2025-06-15 09:54:21 +0200

    Update DefensiveDrivingPage.jsx

--- src/pages/DefensiveDrivingPage.jsx
+++ src/pages/DefensiveDrivingPage.jsx
@@ -43,7 +43,7 @@ const DefensiveDrivingPage = () => {
         </div>
 
         <StyledButton
-          to="/k53-explained/quiz"
+          to="/defensive-driving/quiz"
           asLink
           variant="large"
           className="mt-32 mb-6"
