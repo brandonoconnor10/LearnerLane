5093b9b41c1fe3580adc39101921754b970c798f
Brandon O'Connor <brandonleeoconnor10@gmail.com>
2025-06-15 10:00:32 +0200

    Update VariationsPage.jsx

--- src/pages/VariationsPage.jsx
+++ src/pages/VariationsPage.jsx
@@ -43,7 +43,7 @@ const VariationsPage = () => {
         </div>
 
         <StyledButton
-          to="/k53-explained/quiz"
+          to="/variations/quiz"
           asLink
           variant="large"
           className="mt-32 mb-6"
