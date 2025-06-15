61ade31f000d805a81b086e1fa8bca9bc2aca1da
Brandon O'Connor <brandonleeoconnor10@gmail.com>
2025-06-15 09:54:56 +0200

    Update DutiesOfMotorcyclesPage.jsx

--- src/pages/DutiesOfMotorcyclesPage.jsx
+++ src/pages/DutiesOfMotorcyclesPage.jsx
@@ -43,7 +43,7 @@ const DutiesOfMotorcyclesPage = () => {
         </div>
 
         <StyledButton
-          to="/k53-explained/quiz"
+          to="/duties-relating-to-motorcycles/quiz"
           asLink
           variant="large"
           className="mt-32 mb-6"
