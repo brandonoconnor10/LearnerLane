541fa08093bacfb6b1ad7415c4df7771e829d3d5
Brandon O'Connor <brandonleeoconnor10@gmail.com>
2025-06-17 20:08:56 +0200

    Update Title.jsx

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -1,8 +1,7 @@
-
 const Title = () => {
   return (
     <h1 className="text-5xl font-bold text-white font-orbitron text-center py-6">Learner Lane</h1>
   )
 }
 
-export default Title
+export default Title;
