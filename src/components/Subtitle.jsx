e360c5bbbc5c6269e70779e0bfeda155af43845e
Brandon O'Connor <brandonleeoconnor10@gmail.com>
2025-06-17 20:08:54 +0200

    Update Subtitle.jsx

--- src/components/Subtitle.jsx
+++ src/components/Subtitle.jsx
@@ -1,9 +1,10 @@
-
 const Subtitle = () => {
   return (
-    <p className="text-xl text-gray-light font-inter text-center max-w-2xl mx-auto px-4 mb-8">Welcome to Leaner Lane - your personal guide to acing the K53 Learner's Licence! <br/><br/>
-    Start your journey with easy-to-follow sections covering Rules of The Road, Road Signs and Car Controls.</p>
+    <p className="text-xl text-gray-light font-inter text-center max-w-2xl mx-auto px-4 mb-8">
+      Welcome to Leaner Lane - your personal guide to acing the K53 Learner's Licence! <br /><br />
+      Start your journey with easy-to-follow sections covering Rules of The Road, Road Signs and Car Controls.
+    </p>
   )
 }
 
-export default Subtitle
+export default Subtitle;
