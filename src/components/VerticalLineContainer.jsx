8d3d8409210d8ac8ea75b15c5c8522bc514918e6
Brandon O'Connor <brandonleeoconnor10@gmail.com>
2025-06-17 20:09:01 +0200

    Update VerticalLineContainer.jsx

--- src/components/VerticalLineContainer.jsx
+++ src/components/VerticalLineContainer.jsx
@@ -1,6 +1,3 @@
-
-import React from 'react';
-
 const VerticalLineContainer = ({ children, className = '' }) => {
   return (
     <div className={`flex flex-col items-center relative w-full ${className}`}>
