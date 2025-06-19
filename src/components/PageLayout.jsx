7444e770281c3c19bef2e419e8b8ef15025bf1ee
Brandon O'Connor <brandonleeoconnor10@gmail.com>
2025-06-19 18:38:12 +0200

    Setting up mobile view styling

--- src/components/PageLayout.jsx
+++ src/components/PageLayout.jsx
@@ -1,19 +1,35 @@
+import React, { useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import Title from './Title';
 import Footer from './Footer';
 
 const PageLayout = ({ subtitle, children, contentClassName = 'items-center justify-center' }) => {
+  useEffect(() => {
+    function setVh() {
+      const vh = window.innerHeight * 0.01;
+      document.documentElement.style.setProperty('--vh', `${vh}px`);
+    }
+    setVh();
+    window.addEventListener('resize', setVh);
+    return () => window.removeEventListener('resize', setVh);
+  }, []);
+
   return (
-    <div className="min-h-screen bg-navy-dark text-white flex flex-col items-center justify-start p-4">
+    <div
+      className="bg-navy-dark text-white flex flex-col items-center justify-start p-4"
+      style={{ minHeight: 'calc(var(--vh, 1vh) * 100)' }}
+    >
       <div className="w-full text-center">
         <Link to="/">
           <Title />
         </Link>
         {subtitle && subtitle}
       </div>
-      <div className={`flex-1 flex flex-col w-full ${contentClassName}`}>
+
+      <div className={`flex-grow flex flex-col w-full ${contentClassName}`}>
         {children}
       </div>
+
       <Footer />
     </div>
   );
